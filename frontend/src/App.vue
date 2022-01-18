6376f60db4285ce83a56bb44ebf33f966b4279bd
eora21 <eora21@naver.com>
2022-01-18 16:33:27 +0900

    front-end skeleton

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button-component/>
+  </div>
+</template>
+
+<script>
+import buttonComponent from './components/mag-jang/buttonComponent.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default {
+  name: 'App',
+  components: {
+    buttonComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
