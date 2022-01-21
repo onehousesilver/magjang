8fd73b09c2bea4aab2cfaaebc6beb7368740f140
onehousesilver <onehousesilver@naver.com>
2022-01-21 15:43:17 +0900

    Add: FE-mainpage

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="nav">
+  </div>
+  <router-view />
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Main/EnterInviteCodeBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="input-group input-group-lg"
+    @keyup.enter="member">
+    <input
+      type="text"
+      class="form-control"
+      aria-label="Sizing example input"
+      aria-describedby="inputGroup-sizing-lg"
+      placeholder="초대코드를 입력해주세요" />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.input-group {
+  position: relative;
+  text-align: center;
+  width: 400px;
+  margin: 0 auto;
+}
+</style>
