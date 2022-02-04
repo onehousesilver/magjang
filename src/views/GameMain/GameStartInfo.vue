0c7c03f4c45cd7d5953010c398701ae366d526eb
이소영 <th42500@naver.com>
2022-02-04 14:02:55 +0900

    FIX : 타이머 수정

--- src/views/GameMain/GameStartInfo.vue
+++ src/views/GameMain/GameStartInfo.vue
@@ -49,33 +49,43 @@ export default {
   data() {
     return {
       money : 2000,
-      timeLimit: 20,
+      timeLimit: 10,
       timePassed: 0,
       timerInterval: null,
     };
   },
   computed: {
     timeLeft() {
+      if (this.timeLimit - this.timePassed == 0) {
+        this.skipTimer();
+      }
       return this.timeLimit - this.timePassed
     },
-    formattedTimeLeft() {
-      const timeLeft = this.timeLeft
+    // formattedTimeLeft() {
+    //   const timeLeft = this.timeLeft
 			
-      const minutes = Math.floor(timeLeft / 60)
+    //   const minutes = Math.floor(timeLeft / 60)
 			
-      let seconds = timeLeft % 60
+    //   let seconds = timeLeft % 60
 			
-      if (seconds < 10) {
-        seconds = `0${seconds}`
-      }
+    //   if (seconds < 10) {
+    //     seconds = `0${seconds}`
+    //   }
 			
-      // The output in MM:SS format
-      return `${minutes}:${seconds}`
-    }
+    //   // The output in MM:SS format
+    //   return `${minutes}:${seconds}`
+    // }
   },
   methods: {
     startTimer() {
       this.timerInterval = setInterval(() => (this.timePassed += 1), 1000);
+    },
+    skipTimer() {
+      this.timeLimit = 10,
+      this.timePassed = 0,
+      clearInterval(this.timerInterval);
+      this.timerInterval = null;
+      this.startTimer(); // 타이머 무한 반복하고 싶지 않을 때 주석하기
     }
   },
   mounted() {
