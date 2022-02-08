f1d0756461d26b9915945f88bcc150ea4901d6bf
kst2575 <kst2575@naver.com>
2022-02-08 10:06:56 +0900

    UPDATE: Chat

--- mag_jang/src/main/java/B208/mag_jang/StompWebSocketConfig.java
+++ mag_jang/src/main/java/B208/mag_jang/StompWebSocketConfig.java
@@ -13,7 +13,7 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // WebSocket 또는 SockJS Client가 웹소켓 핸드셰이크 커넥션을 생성할 경로 지정
-        registry.addEndpoint("/stomp/chat").setAllowedOrigins("http://localhost:3000").setHandshakeHandler(new StompHandshakeHandler()).withSockJS();
+        registry.addEndpoint("/stomp/chat").setAllowedOrigins("http://localhost:3000").withSockJS();
 //        registry.addEndpoint("/").setAllowedOrigins("*").withSockJS();
     }
 
