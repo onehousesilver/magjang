94780c1a8d3516929f343f60c5bc6fdd6d7f94ad
eora21 <eora21@naver.com>
2022-02-14 10:26:41 +0900

    TEST: email to maverid in frontend

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,7 +5,7 @@ export default createStore({
     userId: null,  // DB에서 유저 키값
     nickName: null,
     rankPoint: null,
-    email: null,
+    naverId: null,
     winAmount: null,
     gangAmount: null,
     proGangAmount: null,
@@ -15,14 +15,14 @@ export default createStore({
     userId: state => state.userId,
     nickName: state => state.nickName,
     rankPoint: state => state.rankPoint,
-    email: state => state.email,
+    naverId: state => state.naverId,
     winAmount: state => state.winAmount,
     gangAmount: state => state.gangAmount,
     proGangAmount: state => state.proGangAmount,
     lastGenRoom: state => state.lastGenRoom,
     
     isLogined: function(state){
-      if (state.userId && state.email)
+      if (state.userId && state.naverId)
         return true
       return false
     }
@@ -32,7 +32,7 @@ export default createStore({
       state.userId = userdata["userId"]
       state.nickName = userdata["nickName"]
       state.rankPoint = userdata["rankPoint"]
-      state.email = userdata["email"]
+      state.naverId = userdata["naverId"]
       state.winAmount = userdata["winAmount"]
       state.gangAmount = userdata["gangAmount"]
       state.proGangAmount = userdata["proGangAmount"]
@@ -43,8 +43,8 @@ export default createStore({
     }
   },
   actions: {  // mutations 호출, 비동기 가능
-    setUser: function ({commit}, userId, nickName, rankPoint, email, winAmount, gangAmount, proGangAmount, lastGenRoom) {
-      commit('SET_USER', userId, nickName, rankPoint, email, winAmount, gangAmount, proGangAmount, lastGenRoom)
+    setUser: function ({commit}, userId, nickName, rankPoint, naverId, winAmount, gangAmount, proGangAmount, lastGenRoom) {
+      commit('SET_USER', userId, nickName, rankPoint, naverId, winAmount, gangAmount, proGangAmount, lastGenRoom)
     },
     setNickName({commit}, nickName){
       commit('SET_NICKNAME', nickName)
