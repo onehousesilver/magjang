fb3d52db3c7c2658f24bba85547c8e690d60403c
eora21 <eora21@naver.com>
2022-02-18 05:30:39 +0900

    FIX: options

--- frontend/src/components/GameMain/layouts/GameChat.vue
+++ frontend/src/components/GameMain/layouts/GameChat.vue
@@ -234,7 +234,7 @@ export default {
     connect() {
       // 서버에 연결 요청 - StompWebSocketConfig 참조
       const serverURL = "https://localhost:8080/stomp/chat";
-      let socket = new SockJS(serverURL, null, {transports: ["websocket", "xhr-streaming", "xhr-polling"]});
+      let socket = new SockJS(serverURL);
       this.stompClient = Stomp.over(socket);
       console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`);
       this.stompClient.connect(
